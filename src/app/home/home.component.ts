b7b3ef07b0b74d879729a7b53f875d46e2b2e039
Tasos Tsoukalas <tasostsouk.tt@gmail.com>
2019-08-27 16:10:17 +0300

    Added a character sprite that is animated and changed the character related values in order to make sure the character stays inside the window.  Added "keyup" host listener to change the character class to "move" and back.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,15 @@ export class HomeComponent implements OnInit {
   charWidth: number;
   charHeight: number;
   charSpeed: number;
+  scale: number;
 
   constructor() {
-    this.charWidth = 50;
-    this.charHeight = 50;
+    this.charWidth = 20;
+    this.charHeight = 32;
     this.charSpeed = 20;
-    this.positionX = 0;
+    this.positionX = this.charWidth;
     this.positionY = (window.innerHeight - this.charHeight) / 2;
+    this.scale = 2;
   }
 
   ngOnInit() {
@@ -27,38 +29,68 @@ export class HomeComponent implements OnInit {
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
     if (event.key === 'ArrowUp') {
-      if (this.positionY < (window.innerHeight - this.charHeight) - 20) {
+      if (this.positionY < (window.innerHeight - this.charHeight * this.scale) - 20) {
         this.positionY = this.positionY + 20;
+        this.setClass('character', 'characterMove');
       }
     }
 
     if (event.key === 'ArrowDown') {
-      if (this.positionY > 20) {
+      if (this.positionY > 20 + this.charHeight) {
         this.positionY = this.positionY - 20;
+        this.setClass('character', 'characterMove');
       }
     }
 
     if (event.key === 'ArrowLeft') {
-      if (this.positionX > 0) {
+      if (this.positionX - this.charWidth > 0) {
         this.positionX = this.positionX - 20;
+        this.setClass('character', 'characterMove');
       }
     }
 
     if (event.key === 'ArrowRight') {
-      if (this.positionX < (window.innerWidth - this.charWidth) - 20) {
+      if (this.positionX < (window.innerWidth - this.charWidth * this.scale) - 20) {
         this.positionX = this.positionX + 20;
+        this.setClass('character', 'characterMove');
       }
-    } 
+    }
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent2(event: KeyboardEvent) {
+    if (event.key === 'ArrowUp') {
+      this.setClass('characterMove', 'character');
+    }
+
+    if (event.key === 'ArrowDown') {
+      this.setClass('characterMove', 'character');
+    }
+
+    if (event.key === 'ArrowLeft') {
+      this.setClass('characterMove', 'character');
+    }
+
+    if (event.key === 'ArrowRight') {
+      this.setClass('characterMove', 'character');
+    }
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     event.target.innerWidth;
-    if (this.positionX + this.charWidth > window.innerWidth) {
-      this.positionX = window.innerWidth - this.charWidth;
+    if (this.positionX + this.charWidth * this.scale > window.innerWidth) {
+      this.positionX = window.innerWidth - this.charWidth * this.scale;
     }
-    if (this.positionY + this.charHeight > window.innerHeight) {
-      this.positionY = window.innerHeight - this.charHeight;
+    if (this.positionY + this.charHeight * this.scale > window.innerHeight) {
+      this.positionY = window.innerHeight - this.charHeight * this.scale;
+    }
+  }
+
+  setClass(currentClass: string, futureClass: string) {
+    const element = document.getElementsByClassName(currentClass);
+    if (element.length !== 0) {
+      document.getElementsByClassName(currentClass)[0].className = futureClass;
     }
   }
 }
