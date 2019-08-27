21613330c6f1ed99bbbccfe0a323c58260f4729d
Tasos Tsoukalas <tasostsouk.tt@gmail.com>
2019-08-27 16:22:39 +0300

    Updated the project.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to flight-simulator!');
+    expect(page.getTitleText()).toEqual('Welcome to no-going-back!');
   });
 
   afterEach(async () => {
