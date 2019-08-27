00e3b7b4fed59c5a8751a188d424a0e4d603e244
Tasos Tsoukalas <tasostsouk.tt@gmail.com>
2019-08-27 15:09:52 +0300

    Made sure that the character stays inside the window even after it is resized with the use of a window:resize event.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,21 +31,34 @@ export class HomeComponent implements OnInit {
         this.positionY = this.positionY + 20;
       }
     }
+
     if (event.key === 'ArrowDown') {
       if (this.positionY > 20) {
         this.positionY = this.positionY - 20;
       }
     }
+
     if (event.key === 'ArrowLeft') {
       if (this.positionX > 0) {
         this.positionX = this.positionX - 20;
       }
     }
+
     if (event.key === 'ArrowRight') {
       if (this.positionX < (window.innerWidth - this.charWidth) - 20) {
         this.positionX = this.positionX + 20;
       }
+    } 
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    event.target.innerWidth;
+    if (this.positionX + this.charWidth > window.innerWidth) {
+      this.positionX = window.innerWidth - this.charWidth;
     }
-    
+    if (this.positionY + this.charHeight > window.innerHeight) {
+      this.positionY = window.innerHeight - this.charHeight;
+    }
   }
 }
